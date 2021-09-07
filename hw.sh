48f4a55445fefd7b66f0288abf2cdd5886d69f60
velikrgl <velikrgl@yahoo.com>
2021-09-08 02:32:59 +0300

    isPackageInstalled

--- hw.sh
+++ hw.sh
@@ -2,7 +2,23 @@
 #Genel sorgu fonsiyonu 
 genelSorgu(){
 
-    result = $(dpkg -l | wc -l )
+    result =$(dpkg -l | wc -l )
     echo "Sistemde yüklü paket sayısı $result"
 
 }
+
+#if package is installed or not
+
+paketSorgulama(){
+
+isPackageInstalled= $(dpkg -s $1 2>&1)
+isInstalled=$(echo $isPackageInstalled | grep "is not installed")
+
+
+#----
+#----
+}
+
+
+
+
