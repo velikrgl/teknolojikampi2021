b345f0d86b34f1b485665bc8b13d83d550af7bb9
velikrgl <velikrgl@yahoo.com>
2021-09-08 02:33:32 +0300

    dependsCounter

--- hw.sh
+++ hw.sh
@@ -13,7 +13,9 @@ paketSorgulama(){
 
 isPackageInstalled= $(dpkg -s $1 2>&1)
 isInstalled=$(echo $isPackageInstalled | grep "is not installed")
-
+#if-else packet condition
+dependsCount=$(apt-cache depends $1 | grep "Depends:" | wc -l)
+echo "depends :$dependsCount"
 
 #----
 #----
