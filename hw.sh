556b5f0e0f43079a20e69fe846371621f254a46d
velikrgl <velikrgl@yahoo.com>
2021-09-09 22:26:10 +0300

    bağımlılık sayısı belirlenmesi

--- hw.sh
+++ hw.sh
@@ -14,8 +14,18 @@ paketSorgulama(){
 isPackageInstalled= $(dpkg -s $1 2>&1)
 isInstalled=$(echo $isPackageInstalled | grep "is not installed")
 #if-else packet condition
+if [ ${#isInstalled} -eq 0 ]
+echo "Paket sistemde var"
+else
+echo "Paket sistemde yok "
+#cut ile ':' sembolüne kadar olan kısmıları kesip 2. kısmı alıyorum
+depends_packages =$(apt-cache depends $1 | grep "Depends:" | cut -d ":" -f 2)
+
+
+
+
 dependsCount=$(apt-cache depends $1 | grep "Depends:" | wc -l)
-echo "depends :$dependsCount"
+echo "depends sayısı :$dependsCount"
 
 #----
 #----
